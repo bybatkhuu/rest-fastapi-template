160107760f42f8b9e23534d994e9322f204fc0dd
Batkhuu Byambajav <batkhuu10@gmail.com>
2025-10-02 17:51:03 +0900

    script: rename function prefixes for consistency across scripts

--- scripts/docker/docker-entrypoint.sh
+++ scripts/docker/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 echo "[INFO]: Running '${FT_API_SLUG}' docker-entrypoint.sh..."
 
-_doStart()
+_start()
 {
 	# exec python -u ./main.py || exit 2
 	exec python -m api || exit 2
@@ -58,7 +58,7 @@ main()
 	## Parsing input:
 	case ${1:-} in
 		"" | -s | --start | start | --run | run)
-			_doStart;;
+			_start;;
 			# shift;;
 		-b | --bash | bash | /bin/bash)
 			shift
