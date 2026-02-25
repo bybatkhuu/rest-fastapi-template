06fd2eb5334fa129834eb211f71db420208c3456
Batkhuu Byambajav <batkhuu10@gmail.com>
2026-02-25 22:21:32 +0900

    chore: remove unused enums and regex patterns from constants

--- src/api/core/constants/_base.py
+++ src/api/core/constants/_base.py
@@ -6,39 +6,6 @@ ENV_PREFIX_API = f"{ENV_PREFIX}API_"
 API_SLUG = "rest-fastapi-template"
 
 
-class EnvEnum(str, Enum):
-    LOCAL = "LOCAL"
-    DEVELOPMENT = "DEVELOPMENT"
-    TEST = "TEST"
-    DEMO = "DEMO"
-    DOCS = "DOCS"
-    STAGING = "STAGING"
-    PRODUCTION = "PRODUCTION"
-
-
-class WarnEnum(str, Enum):
-    ERROR = "ERROR"
-    ALWAYS = "ALWAYS"
-    DEBUG = "DEBUG"
-    IGNORE = "IGNORE"
-
-
-class HashAlgoEnum(str, Enum):
-    md5 = "md5"
-    sha1 = "sha1"
-    sha224 = "sha224"
-    sha256 = "sha256"
-    sha384 = "sha384"
-    sha512 = "sha512"
-
-
-class ConfigFileFormatEnum(str, Enum):
-    YAML = "YAML"
-    JSON = "JSON"
-    TOML = "TOML"
-    INI = "INI"
-
-
 class HTTPSchemeEnum(str, Enum):
     http = "http"
     https = "https"
@@ -48,9 +15,5 @@ __all__ = [
     "ENV_PREFIX",
     "ENV_PREFIX_API",
     "API_SLUG",
-    "EnvEnum",
-    "WarnEnum",
-    "HashAlgoEnum",
-    "ConfigFileFormatEnum",
     "HTTPSchemeEnum",
 ]
