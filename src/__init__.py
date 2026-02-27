082344b15867916a96f9f485fb899ae5f0eeee86
Batkhuu Byambajav <batkhuu10@gmail.com>
2026-02-28 00:34:10 +0900

    fix: remove unused version import and clean up __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -9,7 +9,3 @@ if os.path.isdir("./src"):
     os.chdir("./src")
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-from .api import __version__  # noqa: E402
-
-__all__ = ["__version__"]
