ccbb3d98a9a837b32b8e712bb9125094e85dfb8b
Batkhuu Byambajav <batkhuu10@gmail.com>
2026-02-28 00:03:28 +0900

    fix: refactor main application structure and move main function to main.py

--- src/api/__main__.py
+++ src/api/__main__.py
@@ -1,25 +1,6 @@
-# Third-party libraries
-from dotenv import load_dotenv
-from fastapi import FastAPI
-
-load_dotenv(override=True)
-
-# Internal modules
-from api.bootstrap import create_app, run_server  # noqa: E402
-from api.logger import logger  # noqa: E402
-
-app: FastAPI = create_app()
-
-
-def main() -> None:
-    """Main function."""
-
-    run_server(app="api.__main__:app")
-    return
-
+from api.logger import logger
+from api.main import main
 
 if __name__ == "__main__":
     logger.info("Starting server from '__main__.py'...")
     main()
-
-__all__ = ["app"]
