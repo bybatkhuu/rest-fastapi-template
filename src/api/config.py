f7b2734303bd6a368cb651713640a2bd0148bbd9
Batkhuu Byambajav <batkhuu10@gmail.com>
2026-02-28 09:56:59 +0900

    fix: refactor load_config function to accept parameters for improved flexibility and type safety

--- src/api/config.py
+++ src/api/config.py
@@ -1,4 +1,7 @@
 import os
+from typing import TypeVar, Any
+
+from pydantic import validate_call
 
 from potato_util.io import read_all_configs
 
@@ -6,20 +9,26 @@ from api.core.constants import ENV_PREFIX_API, API_SLUG
 from api.core.configs import MainConfig
 from api.logger import logger
 
+ConfigType = TypeVar("ConfigType", bound=MainConfig)
 
-def load_config() -> MainConfig:
-    _configs_dir = os.path.join("/etc", API_SLUG)
-    _configs_dir_env = os.getenv(f"{ENV_PREFIX_API}CONFIGS_DIR", "")
+
+@validate_call
+def load_config(
+    configs_dir: str = os.path.join("/etc", API_SLUG),
+    env_name: str = f"{ENV_PREFIX_API}CONFIGS_DIR",
+    config_schema: type[ConfigType] = MainConfig,
+) -> ConfigType:
+    _configs_dir_env = os.getenv(env_name, "")
     if _configs_dir_env:
-        _configs_dir = _configs_dir_env
+        configs_dir = _configs_dir_env
 
-    _config_dict = {}
-    if os.path.isdir(_configs_dir):
-        _config_dict = read_all_configs(configs_dir=_configs_dir)
+    _config_dict: dict[str, Any] = {}
+    if os.path.isdir(configs_dir):
+        _config_dict = read_all_configs(configs_dir=configs_dir)
 
-    _config: MainConfig | None = None
+    _config: ConfigType | None = None
     try:
-        _config = MainConfig(**_config_dict)
+        _config = config_schema(**_config_dict)
     except Exception:
         logger.exception("Failed to load config:")
         raise SystemExit(1)
